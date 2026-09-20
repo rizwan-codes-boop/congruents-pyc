3a6cf63516b34a1be975792715363cf5a3ba07c7
Mirza Rizwan Ali Baig <rizzy@Rizzys-Mac.local>
2026-09-20 13:43:08 +1000

    physics: define Python constants in reference units

---
+++ src/congruents/constants.py
@@ -0,0 +1,26 @@
+"""Native-unit constants frozen to improvised legacy code/physical_constants.h.
+Astropy converts user inputs; it does not silently replace these baseline values.
+"""
+C = 2.99792458e10  # Speed of light [cm/s]
+H = 4.135667696e-24  # Planck constant [GeV s]
+H_ERG = 6.62607015e-27  # Planck constant [erg s]
+KB = 8.617333262e-14  # Boltzmann constant [GeV/K]
+ME = 5.10998950e-4  # Electron rest energy [GeV]
+MP = 9.3827208816e-1  # Proton rest energy [GeV]
+YEAR = 3.15576e7  # Seconds per Julian year
+KB_ERG = 1.380649e-16  # Boltzmann constant [erg/K]
+EULER = 5.7721566490153286e-1  # Euler-Mascheroni constant [dimensionless]
+ME_G = 9.1093837015e-28  # Electron mass [g]
+SIGMA_MB = 6.6524587321e2  # Thomson cross section [mb]
+MB_CM2 = 1.e-27  # cm^2 per millibarn
+ALPHA = 7.2973525693e-3  # Fine-structure constant [dimensionless]
+E_ESU = 4.80320471257e-10  # Elementary charge [Gaussian esu]
+ERG_GEV = 6.241509074e2  # GeV per erg
+GEV_ERG = 1.602176634e-3  # erg per GeV (independently frozen reference value)
+PC = 3.0856775814913673e18  # Centimetres per parsec
+LSUN = 3.828e33 * 6.241509074e2  # Solar luminosity [GeV/s]
+TCMB = 2.7255  # Present-day CMB temperature [K]
+ARAD = 7.5657e-15 * 6.241509074e2  # Blackbody radiation constant [GeV cm^-3 K^-4]
+MH_KG = 1.6735575e-27  # Hydrogen mass [kg]
+MSUN_KG = 1.988409870698051e30  # Solar mass [kg]
+G_PC_MSUN_KMS2 = 4.30091727003628e-3  # Gravity constant [pc (km/s)^2 / Msun]
